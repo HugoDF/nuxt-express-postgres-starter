6950f695f10eaa345118f80592ea872e1574b43b
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-02-24 01:12:58 +0000

    update GitHub link

--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,7 @@
       </h2>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://github.com/HugoDF/nuxt-express-postgres-starter"
           target="_blank"
           class="button--grey"
         >GitHub</a>
